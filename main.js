65ca652e3d833e73836f690092304122f26afd22
Valentina Vargas Patiño <vvargaspr@gmail.com>
2023-02-09 06:41:51 -0500

    actualizacion de archivos css, main js y html

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 // Recordar que el query selector permite seleccionar clases, ids y elementos como en css
 const navEmail = document.querySelector('.nav-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 // Toggle significa intencambiar
 // Llama a la función que dentro ejecuta otra funcion classlist.toggle que quita lo quie sea que tenga la clase inactive cuando se haga el event listener
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
