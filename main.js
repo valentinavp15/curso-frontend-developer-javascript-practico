a2e8e5f2f5bd7417a9dd525cd3c1a60e7b13e132
Valentina Vargas Patiño <vvargaspr@gmail.com>
2023-02-09 07:02:39 -0500

    Cambios agregados

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 // Recordar que el query selector permite seleccionar clases, ids y elementos como en css
 const navEmail = document.querySelector('.nav-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.nav-shopping-cart');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('product-detail');
 
 // Toggle significa intencambiar
 // Llama a la función que dentro ejecuta otra funcion classlist.toggle que quita lo quie sea que tenga la clase inactive cuando se haga el event listener
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive')
+    desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    aside.classList.toggle('inactive');
 }
